141731abec06760de665fb5f41d9dc618d96c9c2
sbartlett97 <sam.bartlett858@gmail.com>
2026-03-18 17:28:38 +0000

    updates

--- src/evaluation/eval_runner.py
+++ src/evaluation/eval_runner.py
@@ -83,7 +83,7 @@ def _generate_responses(
         with torch.no_grad():
             outputs = model.generate(  # type: ignore[union-attr]
                 **inputs,
-                max_new_tokens=256,
+                max_new_tokens=1024,
                 temperature=0.7,
                 top_p=0.9,
                 do_sample=True,
